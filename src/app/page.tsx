4598202dca9ff50749ae048cb85b6913bef12a0a
Spurk <oscartang.dev@gmail.com>
2024-11-14 16:01:39 +1000

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
         </>
       )}
       <div className="mx-2 my-16 flex w-full flex-col justify-between text-center">
-        <h1 className="inline-block bg-gradient-to-b from-yellow-500 via-primary-100 to-red-700 bg-clip-text font-black text-transparent ~mt-0/28 ~leading-[4rem]/[7.5rem] ~/lg:~text-[4.2rem]/[8rem]">
+        <h1 className="inline-block bg-gradient-to-b from-yellow-500 via-primary-100 to-red-700 bg-clip-text font-black text-transparent ~mt-0/28 ~leading-[4.2rem]/[7.8rem] ~/lg:~text-[4.2rem]/[8rem]">
           <button
             className={`cursor-pointer transition-all duration-200 ease-in-out ${wordPressed ? 'scale-105' : ''}`}
             onClick={() => setCursorTrail(!cursorTrail)}
@@ -24,7 +24,7 @@ const Home = () => {
             onMouseUp={() => setWordPressed(false)}
           >
             Word
-          </button>
+          </button>{' '}
           Assassins
         </h1>
         <StartButton />
